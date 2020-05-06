874c8d494afae667d97c7694720686a809bd3fc3
Anton <arnellomutty2@gmail.com>
2020-05-06 12:59:24 +0300

    Added progress

--- src/components/Articles.tsx
+++ src/components/Articles.tsx
@@ -1,9 +1,14 @@
 import React, { FunctionComponent } from 'react'
 import { useArticlesContext } from '../contexts/ArticlesContext'
 import { IArticle } from '../hooks/useArticles'
+import Progress from './Progress'
 
 const Articles: FunctionComponent = ({ children }) => {
   const { articles } = useArticlesContext()
+
+  if (!articles) {
+    return <Progress />
+  }
 
   return (
     <React.Fragment>
---
+++ src/components/Progress.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { makeStyles, createStyles } from '@material-ui/core/styles'
+import LinearProgress from '@material-ui/core/LinearProgress'
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    root: {
+      width: '100%',
+    },
+  })
+)
+
+export default function Progress() {
+  const classes = useStyles()
+
+  return (
+    <div className={classes.root}>
+      <LinearProgress color="secondary"/>
+    </div>
+  )
+}
